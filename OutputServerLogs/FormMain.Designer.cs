5131b099a57a69c4c685c973455c8f5bd927b0b4
alochbaum <alochbaum@mac.com>
2017-02-04 03:44:57 +0000

    Added more controls trying to manage combo box

--- OutputServerLogs/FormMain.Designer.cs
+++ OutputServerLogs/FormMain.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cbFromDir = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.cbToDirectory = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -39,108 +38,144 @@
             this.label4 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnZipTo = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.cbFromDir = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // cbFromDir
-            // 
-            this.cbFromDir.FormattingEnabled = true;
-            this.cbFromDir.Location = new System.Drawing.Point(184, 12);
-            this.cbFromDir.Name = "cbFromDir";
-            this.cbFromDir.Size = new System.Drawing.Size(852, 33);
-            this.cbFromDir.TabIndex = 0;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 13);
+            this.label1.Location = new System.Drawing.Point(14, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(153, 25);
+            this.label1.Size = new System.Drawing.Size(75, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "From Directory";
             // 
             // cbToDirectory
             // 
             this.cbToDirectory.FormattingEnabled = true;
-            this.cbToDirectory.Location = new System.Drawing.Point(184, 51);
+            this.cbToDirectory.Location = new System.Drawing.Point(92, 27);
+            this.cbToDirectory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbToDirectory.Name = "cbToDirectory";
-            this.cbToDirectory.Size = new System.Drawing.Size(852, 33);
+            this.cbToDirectory.Size = new System.Drawing.Size(428, 21);
             this.cbToDirectory.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 52);
+            this.label2.Location = new System.Drawing.Point(14, 27);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(129, 25);
+            this.label2.Size = new System.Drawing.Size(65, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "To Directory";
             // 
             // btnFromDir
             // 
-            this.btnFromDir.Location = new System.Drawing.Point(1042, 12);
+            this.btnFromDir.Location = new System.Drawing.Point(521, 6);
+            this.btnFromDir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnFromDir.Name = "btnFromDir";
-            this.btnFromDir.Size = new System.Drawing.Size(45, 36);
+            this.btnFromDir.Size = new System.Drawing.Size(22, 19);
             this.btnFromDir.TabIndex = 2;
             this.btnFromDir.Text = "...";
             this.btnFromDir.UseVisualStyleBackColor = true;
+            this.btnFromDir.Click += new System.EventHandler(this.btnFromDir_Click);
             // 
             // btnToDir
             // 
-            this.btnToDir.Location = new System.Drawing.Point(1042, 54);
+            this.btnToDir.Location = new System.Drawing.Point(521, 28);
+            this.btnToDir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnToDir.Name = "btnToDir";
-            this.btnToDir.Size = new System.Drawing.Size(45, 36);
+            this.btnToDir.Size = new System.Drawing.Size(22, 19);
             this.btnToDir.TabIndex = 2;
             this.btnToDir.Text = "...";
             this.btnToDir.UseVisualStyleBackColor = true;
+            this.btnToDir.Click += new System.EventHandler(this.btnToDir_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(32, 103);
+            this.label3.Location = new System.Drawing.Point(16, 54);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(101, 25);
+            this.label3.Size = new System.Drawing.Size(51, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "Start Day";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(184, 98);
+            this.dateTimePicker1.Location = new System.Drawing.Point(71, 50);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(398, 31);
+            this.dateTimePicker1.Size = new System.Drawing.Size(201, 20);
             this.dateTimePicker1.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(605, 103);
+            this.label4.Location = new System.Drawing.Point(276, 54);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(259, 25);
+            this.label4.Size = new System.Drawing.Size(127, 13);
             this.label4.TabIndex = 5;
             this.label4.Text = "number of additional days";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(885, 103);
+            this.numericUpDown1.Location = new System.Drawing.Point(407, 54);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 20);
             this.numericUpDown1.TabIndex = 6;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(32, 154);
+            this.dataGridView1.Location = new System.Drawing.Point(16, 80);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(1038, 365);
+            this.dataGridView1.Size = new System.Drawing.Size(526, 150);
             this.dataGridView1.TabIndex = 7;
+            // 
+            // btnZipTo
+            // 
+            this.btnZipTo.Location = new System.Drawing.Point(473, 52);
+            this.btnZipTo.Name = "btnZipTo";
+            this.btnZipTo.Size = new System.Drawing.Size(69, 23);
+            this.btnZipTo.TabIndex = 8;
+            this.btnZipTo.Text = "zip To dir";
+            this.btnZipTo.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 235);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(525, 101);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // cbFromDir
+            // 
+            this.cbFromDir.FormattingEnabled = true;
+            this.cbFromDir.Location = new System.Drawing.Point(92, 6);
+            this.cbFromDir.Margin = new System.Windows.Forms.Padding(2);
+            this.cbFromDir.Name = "cbFromDir";
+            this.cbFromDir.Size = new System.Drawing.Size(428, 21);
+            this.cbFromDir.TabIndex = 0;
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1108, 551);
+            this.ClientSize = new System.Drawing.Size(562, 348);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnZipTo);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label4);
@@ -152,8 +187,11 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbToDirectory);
             this.Controls.Add(this.cbFromDir);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormMain";
             this.Text = "Collect Output Server Logs";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -162,8 +200,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox cbFromDir;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbToDirectory;
         private System.Windows.Forms.Label label2;
@@ -174,6 +210,10 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnZipTo;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ComboBox cbFromDir;
     }
 }
 
--- OutputServerLogs/FormMain.cs
+++ OutputServerLogs/FormMain.cs
@@ -16,5 +16,36 @@ namespace OutputServerLogs
         {
             InitializeComponent();
         }
+
+        private void btnFromDir_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                cbFromDir.Items.Add(folderBrowserDialog1.SelectedPath);
+            }
+        }
+
+        private void btnToDir_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                cbToDirectory.Items.Add(folderBrowserDialog1.SelectedPath);
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+           // if (Settings.Default.cboCollection != null)
+            //    this.cbFromDir.Items.AddRange(Properties.Settings.Default.cboCollectionFrom);
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+
     }
 }
