f2ef4a0043927e6d771d11b47c63ec8e21711fbe
alochbaum <alochbaum@mac.com>
2017-02-05 14:20:32 -0500

    Added Deleting preset

--- OutputServerLogs/FormMain.cs
+++ OutputServerLogs/FormMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,12 +21,7 @@ namespace OutputServerLogs
         public FormMain()
         {
             InitializeComponent();
-            // Preset Values
-            string[] strPresets = new string[2] { "Framework", "output" };
-            string[] strFromDir = new string[2] { "%appdata%\\itx", "c:\\itxLogs" };
-            string[] strToDir = new string[2] { "c:\\CollectLogs", "c:\\CollectLogs" };
-            bool[] blSubDir = new bool[2] { true, true };
-            // Don't need to keep columns
+            // Set up the columns
             DataColumn lName = new DataColumn("Preset", typeof(string));
             DataColumn fromDir = new DataColumn("fromDir", typeof(string));
             DataColumn toDir = new DataColumn("toDir", typeof(string));
@@ -35,15 +31,29 @@ namespace OutputServerLogs
             dtComboBox.Columns.Add(fromDir);
             dtComboBox.Columns.Add(toDir);
             dtComboBox.Columns.Add(SubDir);
-            for (int i = 0; i < strPresets.Length; i++)
+            // see if there is xml file in current directory
+            string strTemp = System.IO.Path.GetDirectoryName(Application.ExecutablePath).ToString() + "\\presets.xml";
+            if (File.Exists(strTemp))
             {
-                DataRow lRow = dtComboBox.NewRow();
-                lRow["Preset"] = strPresets[i];
-                lRow["fromDir"] = strFromDir[i];
-                lRow["toDir"] = strToDir[i];
-                lRow["subDir"] = blSubDir[i];
-                dtComboBox.Rows.Add(lRow);
-            }
+                dtComboBox.ReadXml(strTemp);
+            } // end load default presets
+            else
+            {
+                // Load Default Preset Values
+                string[] strPresets = new string[2] { "Framework", "output" };
+                string[] strFromDir = new string[2] { "%appdata%\\itx", "c:\\itxLogs" };
+                string[] strToDir = new string[2] { "c:\\CollectLogs", "c:\\CollectLogs" };
+                bool[] blSubDir = new bool[2] { true, true };
+                for (int i = 0; i < strPresets.Length; i++)
+                {
+                    DataRow lRow = dtComboBox.NewRow();
+                    lRow["Preset"] = strPresets[i];
+                    lRow["fromDir"] = strFromDir[i];
+                    lRow["toDir"] = strToDir[i];
+                    lRow["subDir"] = blSubDir[i];
+                    dtComboBox.Rows.Add(lRow);
+                }
+            } // end of else create new base presets
             //myDataSet.Tables.Add(dtComboBox);
             // --- Handling the combobox
             cbxPreset.DataSource = dtComboBox.DefaultView;
@@ -62,7 +72,8 @@ namespace OutputServerLogs
                 lRow["toDir"] = myFormAdd.strToDir;
                 lRow["subDir"] = myFormAdd.blSubDir;
                 dtComboBox.Rows.Add(lRow);
-                // add code to set data source
+                // this becomes new last row, but it isn't selected
+                cbxPreset.SelectedIndex = cbxPreset.Items.Count - 1;
             }
         }
 
@@ -75,7 +86,7 @@ namespace OutputServerLogs
 
         private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
         {
-
+            dtComboBox.WriteXml(System.IO.Path.GetDirectoryName(Application.ExecutablePath).ToString() + "\\presets.xml", true);
         }
 
         private void cbxPreset_SelectedIndexChanged(object sender, EventArgs e)
@@ -92,6 +103,18 @@ namespace OutputServerLogs
             {
                 rbSubDirs.Checked = false;
                 rbPrefixName.Checked = true;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(cbxPreset.Items.Count < 2)
+            {
+                MessageBox.Show("You can't delete last preset, you must add one first");
+            }
+            else
+            {
+                dtComboBox.Rows[cbxPreset.SelectedIndex].Delete();
             }
         }
     }
