d8951e79728c5084c3488cf715385f5d6bad9357
alochbaum <alochbaum@mac.com>
2017-02-07 11:28:30 -0500

    version 1.1 now with disappearing buttons

---
+++ FixClipboard/FixClipboard/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FixClipboard
+{
+    public partial class Fix : Form
+    {
+        System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
+
+
+
+        public Fix()
+        {
+            InitializeComponent();
+            string strTemp = Clipboard.GetText();
+            //strTemp = strTemp.Replace("\t", "");
+            strTemp = strTemp.Trim();
+            Clipboard.SetText(strTemp);
+            t.Interval = 100; // specify interval time as you want
+            t.Tick += new EventHandler(timer_Tick);
+            t.Start();
+
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            t.Stop();
+            System.Environment.Exit(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Exit code 0 means it quit fine
+            System.Environment.Exit(0);
+        }
+    }
+}
--- OutputServerLogs/FormMain.cs
+++ OutputServerLogs/FormMain.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace OutputServerLogs
 {
@@ -48,7 +49,7 @@ namespace OutputServerLogs
 
             // Add a button column. 
             DataGridViewButtonColumn buttonColumn =
-                new DataGridViewButtonColumn();
+                new DataGridViewDisableButtonColumn();
             buttonColumn.HeaderText = "Command";
             buttonColumn.Name = "Command";
             buttonColumn.Text = "Get Files";
@@ -190,6 +191,7 @@ namespace OutputServerLogs
 
             }
         }
+        
         private void writeLog(string strIn,string strStyle = "Black")
         {
             // if you use ForeColor it changes whole thing
@@ -210,9 +212,14 @@ namespace OutputServerLogs
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            DataGridViewDisableButtonCell buttonCell;
             if (dataGridView1.Columns[e.ColumnIndex].Name == "Command")
             {
+                writeLog("index " + e.ColumnIndex.ToString());
                 int irow = e.RowIndex;
+                dataGridView1.Rows[irow].Cells[2].Style.BackColor = Color.LightGreen;
+                buttonCell = dataGridView1[0, irow] as DataGridViewDisableButtonCell;
+                buttonCell.Enabled = false;
                 writeLog("Dest Directory: " + dtDataGrid.Rows[irow][1].ToString(), "Green");
                 int i = 0;
                 DateTime dtmWorking = dateTimePicker1.Value;
@@ -292,4 +299,114 @@ namespace OutputServerLogs
             zipStream.Write(bytes, 0, bytes.Length);
         }
     }
+    #region Added Classes to Add Disable Button Functionality to DataGridViewButtonColumn
+    public class DataGridViewDisableButtonColumn : DataGridViewButtonColumn
+    {
+        public DataGridViewDisableButtonColumn()
+        {
+            this.CellTemplate = new DataGridViewDisableButtonCell();
+        }
+    }
+
+    public class DataGridViewDisableButtonCell : DataGridViewButtonCell
+    {
+        private bool enabledValue;
+        public bool Enabled
+        {
+            get
+            {
+                return enabledValue;
+            }
+            set
+            {
+                enabledValue = value;
+            }
+        }
+
+        private bool drawDisabled;
+        private bool DrawDisabled
+        {
+            get
+            {
+                return drawDisabled;
+            }
+            set
+            {
+                drawDisabled = value;
+            }
+        }
+
+        // Override the Clone method so that the Enabled property is copied.
+        public override object Clone()
+        {
+            DataGridViewDisableButtonCell cell = (DataGridViewDisableButtonCell)base.Clone();
+            cell.Enabled = this.Enabled;
+
+            return cell;
+        }
+
+        // By default, enable the button cell.
+        public DataGridViewDisableButtonCell()
+        {
+            this.enabledValue = true;
+        }
+
+        protected override void Paint(Graphics graphics,
+                                      Rectangle clipBounds, Rectangle cellBounds, int rowIndex,
+                                      DataGridViewElementStates elementState, object value,
+                                      object formattedValue, string errorText,
+                                      DataGridViewCellStyle cellStyle,
+                                      DataGridViewAdvancedBorderStyle advancedBorderStyle,
+                                      DataGridViewPaintParts paintParts)
+        {
+            // The button cell is disabled, so paint the border,
+            // background, and disabled button for the cell.
+            if (!this.enabledValue)
+            {
+                // Draw the cell background, if specified.
+                if ((paintParts & DataGridViewPaintParts.Background) == DataGridViewPaintParts.Background)
+                {
+                    SolidBrush cellBackground = new SolidBrush(cellStyle.BackColor);
+
+                    graphics.FillRectangle(cellBackground, cellBounds);
+                    cellBackground.Dispose();
+                }
+
+                //// Draw the cell borders, if specified.
+                if ((paintParts & DataGridViewPaintParts.Border) == DataGridViewPaintParts.Border)
+                {
+                    PaintBorder(graphics, clipBounds, cellBounds, cellStyle, advancedBorderStyle);
+                }
+
+                // Check wether to draw the disabled control or not.
+                if (drawDisabled)
+                {
+                    // Calculate the area in which to draw the button.
+
+                    Rectangle buttonArea = cellBounds;
+                    Rectangle buttonAdjustment = this.BorderWidths(advancedBorderStyle);
+                    buttonArea.X += buttonAdjustment.X;
+                    buttonArea.Y += buttonAdjustment.Y;
+                    buttonArea.Height -= buttonAdjustment.Height;
+                    buttonArea.Width -= buttonAdjustment.Width;
+
+                    // Draw the disabled button.
+                    ButtonRenderer.DrawButton(graphics, buttonArea, PushButtonState.Disabled);
+
+                    // Draw the disabled button text.
+                    if (this.FormattedValue is String)
+                    {
+                        TextRenderer.DrawText(graphics, (string)this.FormattedValue, this.DataGridView.Font, buttonArea, SystemColors.GrayText);
+                    }
+                }
+            }
+            else
+            {
+                // The button cell is enabled, so let the base class
+                // handle the painting.
+                base.Paint(graphics, clipBounds, cellBounds, rowIndex, elementState, value, formattedValue, errorText, cellStyle, advancedBorderStyle, paintParts);
+            }
+        }
+    }
+    #endregion
 }
